a1e8fb5f45d0daf40948d2883cc51eac33e154e4
rodrigom68 <104014345+rodrigom68@users.noreply.github.com>
2023-06-28 08:48:13 -0300

    testando o comando PERFORM

---
+++ teste07.cbl
@@ -0,0 +1,14 @@
+      ******************************************************************
+      * Author: Rodrigo Ferreira da Silva
+      * Date: 04/06/2023
+      * Purpose: aprendizado
+      * Tectonics: cobc
+      ******************************************************************
+      PROGRAM-ID. PROGCOMM.
+      DATA DIVISION.
+      FILE SECTION.
+      WORKING-STORAGE SECTION.
+      PROCEDURE DIVISION.
+
+      PERFORM P3
+
